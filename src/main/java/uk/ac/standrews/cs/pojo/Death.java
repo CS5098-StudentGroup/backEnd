b747083b89a224aba19dfb95082af1ff304ae1b9
dongyaoliu <liudongyao0108@gmail.com>
2021-07-17 11:09:38 +0100

    update

--- src/main/java/uk/ac/standrews/cs/pojo/Death.java
+++ src/main/java/uk/ac/standrews/cs/pojo/Death.java
@@ -9,7 +9,9 @@ import org.springframework.data.neo4j.core.schema.GeneratedValue;
 import org.springframework.data.neo4j.core.schema.Id;
 import org.springframework.data.neo4j.core.schema.Node;
 import org.springframework.data.neo4j.core.schema.Property;
+import org.springframework.stereotype.Component;
 
+@Component
 @Getter
 @Setter
 @AllArgsConstructor
@@ -18,13 +20,22 @@ import org.springframework.data.neo4j.core.schema.Property;
 public class Death {
     @Id
     @GeneratedValue
-    private Long id;
+    private Long query_number;
+
+    @Property("STORR_ID")
+    private Long storr_id;
+
+    @Property("ORIGINAL_ID")
+    private String original_id;
 
     @Property("SURNAME")
     private String surName;
 
     @Property("FORENAME")
     private String foreName;
+
+    @Property("DATE_OF_BIRTH")
+    private String birth;
 
     @Property("Death_YEAR")
     private String deathYear;
@@ -34,5 +45,9 @@ public class Death {
 
     @Property("DEATH_DAY")
     private String deathDay;
+
+    @Property("PLACE_OF_DEATH")
+    private String deathPlace;
+
 
 }
