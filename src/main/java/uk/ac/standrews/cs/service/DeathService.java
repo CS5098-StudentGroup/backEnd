39ef713ae14e7d562578fb1d9b9d8b242aeb1905
dongyaoliu <liudongyao0108@gmail.com>
2021-07-18 16:54:36 +0100

    update

--- src/main/java/uk/ac/standrews/cs/service/DeathService.java
+++ src/main/java/uk/ac/standrews/cs/service/DeathService.java
@@ -5,5 +5,5 @@ import java.util.List;
 
 
 public interface DeathService {
-    List<Death> findByName (String surName, String foreName, String gender);
+    List<Death> findByName (String surName, String foreName, String gender, String dateOfBirth, String dateOfDeath, String dateOfMarriage);
 }
